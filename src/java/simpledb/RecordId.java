420b4971bada9e735ede91d5d09eb03c4870ebf1
MuhammadRazaKhan <RazaurRehman@gmail.com>
2015-11-02 23:13:56 -0800

    Added RecordId Code

--- src/java/simpledb/RecordId.java
+++ src/java/simpledb/RecordId.java
@@ -19,8 +19,13 @@ public class RecordId implements Serializable {
      * @param tupleno
      *            the tuple number within the page.
      */
+    private PageId pid;
+    private int tupleno;
+    
     public RecordId(PageId pid, int tupleno) {
         // some code goes here
+    	this.pid=pid;
+    	this.tupleno=tupleno;
     }
 
     /**
@@ -28,7 +33,7 @@ public class RecordId implements Serializable {
      */
     public int tupleno() {
         // some code goes here
-        return 0;
+        return this.tupleno;
     }
 
     /**
@@ -36,7 +41,7 @@ public class RecordId implements Serializable {
      */
     public PageId getPageId() {
         // some code goes here
-        return null;
+        return this.pid;
     }
 
     /**
@@ -48,7 +53,16 @@ public class RecordId implements Serializable {
     @Override
     public boolean equals(Object o) {
         // some code goes here
-        throw new UnsupportedOperationException("implement this");
+        boolean ret=false;
+        if (o instanceof RecordId){
+        	RecordId passedRecord=(RecordId)o;
+        	if ((this.pid==passedRecord.pid)&&(this.tupleno==passedRecord.tupleno))
+        		ret=true;
+        	else
+        		ret=false;
+        }
+        
+        return ret;
     }
 
     /**
@@ -60,8 +74,10 @@ public class RecordId implements Serializable {
     @Override
     public int hashCode() {
         // some code goes here
-        throw new UnsupportedOperationException("implement this");
-
+        int pid_hash = this.pid.hashCode();
+        int tupleno_hash = (""+this.tupleno).hashCode();
+        
+        return pid_hash+tupleno_hash;
     }
 
 }
